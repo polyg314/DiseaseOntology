954e58bd43ce0e9a264c0207a3490c8da41320d2
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-30 18:25:38 -0700

    obo

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from biothings import config
 logging = config.logger
 
 from .networkx import networkx
-import obonet_all_functions
+import obonet_all_functions.py
 import re
 import json
 import requests
