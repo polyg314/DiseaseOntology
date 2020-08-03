61230b4d498c8e7a13e5d4b363ac54c00207f4c1
Paul Gaudin <paulgaudin314@gmail.com>
2020-08-03 13:38:27 -0700

    added networx and obonet to requires list

--- parser.py
+++ parser.py
@@ -4,8 +4,8 @@ from biothings.utils.dataload import dict_convert, dict_sweep
 from biothings import config
 logging = config.logger
 
-from .networkx import networkx
-from . import obonet 
+import networkx
+import obonet as obonet
 import re
 import json
 import requests
